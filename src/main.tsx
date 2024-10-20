5bd061674f3164c27015d576e24576eb7e957c94
Atharv Lingayat <atharvofficial7@gmail.com>
2024-10-20 20:41:44 +0530

    removed redundant packages

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,12 @@
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import {DndProvider} from "react-dnd"
-import {HTML5Backend} from "react-dnd-html5-backend"
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './redux/store.ts'
 createRoot(document.getElementById('root')!).render(
   
   <Provider store={store}>
-    <DndProvider backend={HTML5Backend}>
       <App />
-    </DndProvider>
   </Provider>
 )
